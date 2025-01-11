26c4fdb05e9ffe32358ff504da06b8846efd92d1
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-11 10:18:27 +0900

    adding tags related to the todo_list

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 # Voice Assistant in Python
 from neuralintents import GenericAssistant
 import speech_recognition 
-import pyttsx3 as tts
+import pyttsx3 as tts 
 import sys
 
+recognizer = speech_recognition.Recognizer()
+speaker = tts.init()
+speaker.setProperty('rate', 150) # Speech Speed
+
+todo_list = ["Go Shopping", "Clean Room", "Study Korean"]
+
+assistant = GenericAssistant('assistant_intents.json')
+assistant.train_model()
+assistant.request("Hello")
